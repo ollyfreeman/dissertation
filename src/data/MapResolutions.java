060dc5b897ee75d425796198f91a6bab8c5761b1
Olly Freeman <of209@cam.ac.uk>
2013-12-09 16:04:15 +0000

    Added a 'docs' folder with PDFs of algorithms that I'm considering implementing

---
+++ src/data/MapResolutions.java
@@ -0,0 +1,29 @@
+package data;
+
+public class MapResolutions {
+	
+	private static final String[] mapResolutionArray = {"25x50", "50x50", "50x100", "100x100", "100x200", "200x200" , "200x400", "400x400", "400x800", "800x800"};
+	
+	private static final String[] brushResolutionArray = {"64","32","16","8","4","2","1"};
+	
+	public static String[] getMapResolutionArray() {
+		return mapResolutionArray;
+	}
+	
+	public static String[] getBrushResolutionArray() {
+		return brushResolutionArray;
+	}
+	
+	public static int getResolutionFromMapHeight(int height) {
+		switch (height) {
+		case 800: 	return 1;
+		case 400:	return 2;
+		case 200:	return 4;
+		case 100:	return 8;
+		case 50:	return 16;
+		case 25:	return 32;
+		default:	return 0;
+		}
+	}
+
+}
